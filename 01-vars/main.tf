cca3213757b49856ee09211a295ba6672de0f0bc
shuja-git <shuja.devops@gmail.com>
2024-02-01 16:01:48 +0530

    printing list values

--- 01-vars/main.tf
+++ 01-vars/main.tf
@@ -1,3 +1,6 @@
 output "sample1" {
   value = var.sample1
 }
+output "list" {
+  value = var.sample_list.*
+}
--- 01-vars/var.tf
+++ 01-vars/var.tf
@@ -1,3 +1,11 @@
 variable "sample1" {
   default = "Hello from sample1"
 }
+variable "sample_list" {
+  default = [
+    "Shuja",
+    "Luqman",
+    100,
+    true
+  ]
+}
