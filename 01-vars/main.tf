647c0017c585a09793021c4bd845c9910f965757
shuja-git <shuja.devops@gmail.com>
2024-02-10 17:41:42 +0530

    comment all values

--- 01-vars/main.tf
+++ 01-vars/main.tf
@@ -1,9 +1,9 @@
-output "sample1" {
-  value = var.sample1
-}
-output "list" {
-  value = var.sample_list.*
-}
-output "list_of_maps" {
-  value = var.list_of_map.*.course_name
-}
+#output "sample1" {
+#  value = var.sample1
+#}
+#output "list" {
+#  value = var.sample_list.*
+#}
+#output "list_of_maps" {
+#  value = var.list_of_map.*.course_name
+#}
