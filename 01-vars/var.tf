3052464110dde698088aff42843b2fe9ce97882e
shuja-git <shuja.devops@gmail.com>
2024-02-10 17:39:51 +0530

    printing map values

--- 01-vars/var.tf
+++ 01-vars/var.tf
@@ -1,23 +1,38 @@
-variable "sample1" {
-  default = "Hello from sample1"
-}
-variable "sample_list" {
-  default = [
-    "Shuja",
-    "Luqman",
-    100,
-    true
-  ]
-}
-variable "list_of_map" {
-  default = [
-    {
-      course_name = "aws"
-      trainer_name = "Shuja"
-    },
-    {
-      course_name = "devops"
-      trainer_name = "Luqman"
+#variable "sample1" {
+#  default = "Hello from sample1"
+#}
+#variable "sample_list" {
+#  default = [
+#    "Shuja",
+#    "Luqman",
+#    100,
+#    true
+#  ]
+#}
+#variable "list_of_map" {
+#  default = [
+#    {
+#      course_name = "aws"
+#      trainer_name = "Shuja"
+#    },
+#    {
+#      course_name = "devops"
+#      trainer_name = "Luqman"
+#    }
+#  ]
+#}
+variable "d3" {
+  default = {
+    apple = {
+      name = "apple"
+      count = 2
     }
-  ]
+    banana ={
+      name = "banana"
+      count = 5
+    }
+  }
+}
+resource "null_resource" "nothing" {
+  for_each = var.d3
 }
