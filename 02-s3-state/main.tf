73c13e3366fd3ca0296980fac549574259ce51bf
shuja-git <shuja.devops@gmail.com>
2024-02-10 13:54:24 +0530

    length

--- 02-s3-state/main.tf
+++ 02-s3-state/main.tf
@@ -7,8 +7,12 @@ terraform {
 }
 
 resource "aws_instance" "cart" {
-  count = 5
+  count =        length(var.instances)
   ami           = "ami-0f3c7d07486cad139"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-08f41a3b66746e56a"]
+}
+
+variable "instances" {
+  default = ["cart","catalogue","shipping","payment","user"  ]
 }
