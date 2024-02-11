8b3fd230c52b34d43541c96454b1d84a82245c88
shuja-git <shuja.devops@gmail.com>
2024-02-11 15:24:52 +0530

    condition example2

--- 01-vars/04-conditions/main.tf
+++ 01-vars/04-conditions/main.tf
@@ -1,6 +1,8 @@
 resource "aws_instance" "ec2-instance" {
+  count = var.create_instance == true ? 1 : 0
   ami = "ami-0f3c7d07486cad139"
   instance_type =  var.instance_type == "" ? "t2.micro" : var.instance_type
   vpc_security_group_ids = ["sg-08f41a3b66746e56a"]
 }
 variable "instance_type" {}
+variable "create_instance" {}
